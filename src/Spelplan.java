f2bec3b4ec86b157cc183c150c4e5779dea6b1d1
william.grenback <william.grenback@elev.ga.ntig.se>
2020-05-05 11:22:41 +0200

    utvecklat min spelplan lite mer

--- src/Spelplan.java
+++ src/Spelplan.java
@@ -1,13 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
 public class Spelplan {
     private int [] spelplan;
     private int width;
     private int height;
 
-    public Spelplan(int w, int h) {
-        this.width = w;
-        this.height = h;
-        spelplan = new int [w*h];
-    }
+
 
     public int getWidth() {
         return width;
@@ -20,6 +22,31 @@ public class Spelplan {
     public static void main (String[] args) {
     }
 
+    private int[] pixels;
+    private int scale = 4;
+    private JFrame frame;
+    private BufferedImage image;
+
+    private Spelplan w;
+
+    public Spelplan(int w, int h) {
+        this.width = w;
+        this.height = h;
+        spelplan = new int [w*h];
+        image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        Dimension size = new Dimension(scale*w, scale*h);
+        setPreferredSize(size);
+        frame = new JFrame();
+        this.w = new Spelplan(w,h);
+    }
+    private void draw() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+    }
 
 
 }
