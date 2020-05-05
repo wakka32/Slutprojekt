550c988007b8f62460b9b014512c8f11b8eb1a98
william.grenback <william.grenback@elev.ga.ntig.se>
2020-05-05 10:05:54 +0200

    Börjat med kod

--- src/Spelplan.java
+++ src/Spelplan.java
@@ -1,3 +1,28 @@
 public class Spelplan {
-    
+    private int [] spelplan;
+
+    public Spelplan() {
+        spelplan = new int [100];
+    }
+    public static void main (String[] args) {
+    }
+    public class World {
+        private int width;
+        private int height;
+
+        public World(int w, int h) {
+            this.width = w;
+            this.height = h;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+       }
+       
+
 }
