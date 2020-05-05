85b6119533f892cee72c05c523c0a83b4c418709
william.grenback <william.grenback@elev.ga.ntig.se>
2020-05-05 10:22:25 +0200

    Börjat med spelplanen

--- src/Spelplan.java
+++ src/Spelplan.java
@@ -1,28 +1,25 @@
 public class Spelplan {
     private int [] spelplan;
+    private int width;
+    private int height;
 
-    public Spelplan() {
-        spelplan = new int [100];
+    public Spelplan(int w, int h) {
+        this.width = w;
+        this.height = h;
+        spelplan = new int [w*h];
     }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
     public static void main (String[] args) {
     }
-    public class World {
-        private int width;
-        private int height;
 
-        public World(int w, int h) {
-            this.width = w;
-            this.height = h;
-        }
 
-        public int getWidth() {
-            return width;
-        }
-
-        public int getHeight() {
-            return height;
-        }
-       }
-       
 
 }
