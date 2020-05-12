e47ec9e9dbc6a53f556f605f933ce8349a54f7a5
william.grenback <william.grenback@elev.ga.ntig.se>
2020-05-12 11:23:10 +0200

    La även till fartyg, fick hjälp av en kompis samt en video, är detta okej att göra?

--- src/Fartyget.java
+++ src/Fartyget.java
@@ -1,3 +1,27 @@
 public class Fartyget {
+   private int [] location;
+    private int numOfHits;
+
+    public void setLocations(int [] loc){
+        location = loc;
+    }
+
+    public String checkGuess(String Userguess){
+
+        String result = "miss";
+
+        int guess = Integer.parseInt(Userguess);
+
+        for (int loc : location){
+            if (guess == loc){
+            numOfHits++;
+            result = numOfHits == location.length ? "träff" : "sänkt";
+            break;
+        }
+    }
+    System.out.println(result);
+
+        return result;
+    }
 
 }
